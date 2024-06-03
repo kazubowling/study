6f067e76170d7dc3f17fb7dbe6ff1020c90749cc
kazubowling <kazubowling300@gmail.com>
2024-06-03 17:31:57 +0900

    第６章 サンプルコード

--- Study1/dynamic.cs
+++ Study1/dynamic.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Dynamic;
+﻿//using System;
+//using System.Dynamic;
 
-class MainClass
-{
-    static void Main(string[] args)
-    {
-        dynamic pet = new ExpandoObject();
-        pet.name = "Taro";
-        pet.age = 10;
-        Console.WriteLine("名前：" + pet.name + "年齢：" + pet.age);
-    }
-}
+//class MainClass
+//{
+//    static void Main(string[] args)
+//    {
+//        dynamic pet = new ExpandoObject();
+//        pet.name = "Taro";
+//        pet.age = 10;
+//        Console.WriteLine("名前：" + pet.name + "年齢：" + pet.age);
+//    }
+//}
---
+++ Study1/sample6.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace konchu
+{
+    // 昆虫クラス
+    abstract class Insect
+    {
+        public void grow()
+        {
+            tamago();
+            youchu();
+            sanagi();
+            seichu();
+        }
+        protected abstract void tamago();
+        protected abstract void youchu();
+        protected abstract void sanagi();
+        protected abstract void seichu();
+    }
+
+    // 蝶クラス
+    class Butterfly : Insect
+    {
+        protected override void tamago()
+        {
+            Console.WriteLine("卵です");
+        }
+        protected override void youchu()
+        {
+            Console.WriteLine("イモムシのような幼虫です");
+        }
+        protected override void sanagi()
+        {
+            Console.WriteLine("さなぎになりました");
+        }
+        protected override void seichu()
+        {
+            Console.WriteLine("成虫になりました！");
+        }
+    }
+
+    // バッタクラス
+    class Grasshopper : Insect
+    {
+        protected override void tamago()
+        {
+            Console.WriteLine("卵です");
+        }
+        protected override void youchu()
+        {
+            Console.WriteLine("幼虫です");
+            Console.WriteLine("脱皮を繰り返します");
+        }
+        protected override void sanagi()
+        {
+            // バッタにさなぎの状態はありません
+        }
+        protected override void seichu()
+        {
+            Console.WriteLine("成虫になりました！");
+        }
+    }
+
+    // Main() メソッドのクラス
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("[蝶の生態]");
+            Butterfly butterfly = new Butterfly();
+            butterfly.grow();
+            Console.WriteLine("\n[バッタの生態]");
+            Grasshopper grasshopper = new Grasshopper();
+            grasshopper.grow();
+        }
+    }
+}
