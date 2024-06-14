ee2024be32bb685804d7e35e04ec29d94c97cc76
Kazuki <kazubowling300@gmail.com>
2024-06-14 19:35:29 +0900

    文字の書き出し

---
+++ Study1/WriteFile.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.IO;
+
+//class WriteText
+//{
+//    static void Main()
+//    {
+//        FileStream fs;
+//        try
+//        {
+//            fs = new FileStream("file2.txt", FileMode.Create);
+//        }
+//        catch (IOException)
+//        {
+//            Console.WriteLine("ファイルを開けません。");
+//            return;
+//        }
+
+//        StreamWriter w = new StreamWriter(fs);
+//        w.Write("ファイルに書き込みました。");
+
+//        w.Close();
+//    }
+//}
