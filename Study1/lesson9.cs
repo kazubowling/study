0c31d730af1b32d9a4b728f56f6cbf519100c4b9
kazubowling <kazubowling300@gmail.com>
2024-07-08 14:19:11 +0900

    デリゲート

--- Study1/lesson9.cs
+++ Study1/lesson9.cs
@@ -6,20 +6,44 @@ using System.Threading.Tasks;
 
 namespace Study1
 {
-    class Connector<T>
-    {
-        public string connect (T a, T b)
-        {
-            return a.ToString() + b.ToString();
-        }
-    }
+    //class Connector<T>
+    //{
+    //    public string connect (T a, T b)
+    //    {
+    //        return a.ToString() + b.ToString();
+    //    }
+    //}
 
-    class Connector2
+    //class Connector2
+    //{
+    //    static void Main()
+    //    {
+    //        Connector<int> connector = new Connector<int>();
+    //        Console.WriteLine(connector.connect(46, 49));
+    //    }
+    //}
+
+    delegate int DelegMsgs();
+
+    class DelegSample
     {
+        static int print1()
+        {
+            Console.WriteLine("print1");
+            return 1;
+        }
+        static int print2()
+        {
+            Console.WriteLine("print2");
+            return 2;
+        }
         static void Main()
         {
-            Connector<int> connector = new Connector<int>();
-            Console.WriteLine(connector.connect(46, 49));
+            DelegMsgs deleg;
+            deleg = new DelegMsgs(print1);
+            deleg += new DelegMsgs(print2);
+            int n = deleg();
+            Console.WriteLine(n);
         }
     }
 }
