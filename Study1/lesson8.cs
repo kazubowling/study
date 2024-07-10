6069d9359d082926910a815b1b1c14951bba3dff
Kazuki <kazubowling300@gmail.com>
2024-07-10 20:51:34 +0900

    実用的なクラス

--- Study1/lesson8.cs
+++ Study1/lesson8.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 //class A
 //{
@@ -10,25 +11,43 @@
 
 class FormatSample
 {
-    static void Main()
+    //static void Main()
+    //{
+    //    Console.WriteLine("C {0, 6:C}", 1234);
+    //    Console.WriteLine("D {0, 6:D}", 123);
+    //    Console.WriteLine("E {0, 6:E}", 123.45);
+    //    Console.WriteLine("F {0, 6:F}", 123);
+    //    Console.WriteLine("G {0, 6:G}", 123);
+    //    Console.WriteLine("N {0, 10:N}", 123456);
+    //    Console.WriteLine("P {0, 6:P}", 123);
+    //    Console.WriteLine("X {0, 6:X}", 123);
+
+    //    int a = 10;
+    //    double b = 20.5;
+    //    //Console.WriteLine("{0} + {1} は、{2}です。", a, b, 1 + b);
+
+    //    Console.WriteLine($"{a} + {b}は、{a + b}です。");
+
+    //    string a;
+    //    a = String.Format("{0}の{1}", "C#", "絵本");
+    //    Console.WriteLine(a);
+
+    //    Regex r;
+    //    r = new Regex("C.*の絵本");
+    //    Match m = r.Match("これはC#の絵本です");
+    //    Console.WriteLine(m.Index + "番目にC#の記述があります");
+    //    Console.WriteLine(m.Value);
+    //    Console.WriteLine("Valueの長さは" + m.Length + "個分です");
+    //}
+
+    class DTSample
     {
-        //Console.WriteLine("C {0, 6:C}", 1234);
-        //Console.WriteLine("D {0, 6:D}", 123);
-        //Console.WriteLine("E {0, 6:E}", 123.45);
-        //Console.WriteLine("F {0, 6:F}", 123);
-        //Console.WriteLine("G {0, 6:G}", 123);
-        //Console.WriteLine("N {0, 10:N}", 123456);
-        //Console.WriteLine("P {0, 6:P}", 123);
-        //Console.WriteLine("X {0, 6:X}", 123);
+        static void Main()
+        {
+            DateTime dt = DateTime.Now;
 
-        //int a = 10;
-        //double b = 20.5;
-        ////Console.WriteLine("{0} + {1} は、{2}です。", a, b, 1 + b);
-
-        //Console.WriteLine($"{a} + {b}は、{a + b}です。");
-
-        string a;
-        a = String.Format("{0}の{1}", "C#", "絵本");
-        Console.WriteLine(a);        
+            Console.WriteLine("今日は{0}年{1}月{2}日({3})で、", dt.Year, dt.Month, dt.Day, dt.DayOfWeek);
+            Console.WriteLine("現在{0}時{1}分{2}秒です", dt.Hour, dt.Minute, dt.Second);
+        }
     }
 }
